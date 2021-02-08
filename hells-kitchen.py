41c08cc82b75a6f67ca05f124bac8e795601905f
xalxnder <xalexander51@gmail.com>
2021-02-08 18:45:38 -0500

    Add description.

--- hells-kitchen.py
+++ hells-kitchen.py
@@ -1,3 +1,11 @@
+'''
+Description:
+Gordon Ramsay shouts. He shouts and swears. There may be something wrong with him.
+Anyway, you will be given a string of four words. Your job is to turn them in to Gordon language.
+
+Rules:
+Obviously the words should be Caps, Every word should end with '!!!!', Any letter 'a' or 'A' should become '@', Any other vowel should become '*'.
+'''
 vowels = ['e','i','o','u']
 def gordon(a):
     arr = a.split()
