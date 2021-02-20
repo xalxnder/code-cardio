1f8969f00139c45924b0d2ebef15ef6fb4e38fbe
xalxnder <xalexander51@gmail.com>
2021-02-20 18:33:02 -0500

    Add clean up after dog problem

---
+++ clean_up_after_dog.py
@@ -0,0 +1,40 @@
+'''
+Given a 2D array to represent your garden, you must find and collect all of the dog cr@p - represented by '@'.
+
+You will also be given the number of bags you have access to (bags), and the capactity of a bag (cap). If there are no bags then you can't pick anything up, so you can ignore cap.
+
+You need to find out if you have enough capacity to collect all the cr@p and make your garden clean again.
+
+If you do, return 'Clean', else return 'Cr@p'.
+
+Watch out though - if your dog is out there ('D'), he gets very touchy about being watched. If he is there you need to return 'Dog!!'.
+
+For example:
+
+x=
+[[_,_,_,_,_,_]
+[_,_,_,_,@,_]
+[@,_,_,_,_,_]]
+
+bags = 2, cap = 2
+
+return --> 'Clean'
+'''
+
+def crap(garden, bags, cap):
+    poop = 0
+    for lists in garden:
+        for space in lists:
+            if space == '@':
+                poop += 1
+            elif space == 'D':
+                return 'Dog!!'
+    if bags * cap >= poop:
+        return 'Clean'
+    elif bags * cap <= poop:
+        return 'Cr@p'
+    
+
+
+
+print(crap([['@','@'], ['@','@'], ['@','@']], 3, 2))
